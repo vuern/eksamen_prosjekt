754347e3b544865abc2d4cb29de3b3cef4bd2814
Alexander Ormseth <a_ormseth@hotmail.com>
2021-05-18 15:41:49 +0200

    Formatting

--- js/store/Restaurants.js
+++ js/store/Restaurants.js
@@ -1,31 +1,31 @@
 const restaurants = [
-    {
-        restaurantId: 1,
-        location: "Karl Johan",
-        imgSrc: "",
-        openingHours: "",
-        telephone: 71582859,
-    },
-    {
-        restaurantId: 2,
-        location: "Operaen",
-        imgSrc: "",
-        openingHours: "",
-        telephone: 92938283,
-    },
-    {
-        restaurantId: 3,
-        location: "Stovner",
-        imgSrc: "",
-        openingHours: "",
-        telephone: 60003005,
-    },
-    {
-        restaurantId: 4,
-        location: "Bislett",
-        imgSrc: "",
-        openingHours: "",
-        telephone: 90901090,
-    },
+  {
+    restaurantId: 1,
+    location: "Karl Johan",
+    imgSrc: "",
+    openingHours: "",
+    telephone: 71582859,
+  },
+  {
+    restaurantId: 2,
+    location: "Operaen",
+    imgSrc: "",
+    openingHours: "",
+    telephone: 92938283,
+  },
+  {
+    restaurantId: 3,
+    location: "Stovner",
+    imgSrc: "",
+    openingHours: "",
+    telephone: 60003005,
+  },
+  {
+    restaurantId: 4,
+    location: "Bislett",
+    imgSrc: "",
+    openingHours: "",
+    telephone: 90901090,
+  },
 ];
 export default restaurants;

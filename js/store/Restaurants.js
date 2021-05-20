4ce840d9b6599add7b5d3e1d740cc5a1f7259c69
Alpha Jalloh <alphamartinj@gmail.com>
2021-05-20 17:32:29 +0200

    Bulma
    Lagt inn Bulma og navn på restauranter.

--- js/store/Restaurants.js
+++ js/store/Restaurants.js
@@ -1,31 +1,42 @@
-const restaurants = [
-  {
-    restaurantId: 1,
-    location: "Karl Johan",
-    imgSrc: "",
-    openingHours: "",
-    telephone: 71582859,
-  },
-  {
-    restaurantId: 2,
-    location: "Operaen",
-    imgSrc: "",
-    openingHours: "",
-    telephone: 92938283,
-  },
-  {
-    restaurantId: 3,
-    location: "Stovner",
-    imgSrc: "",
-    openingHours: "",
-    telephone: 60003005,
-  },
-  {
-    restaurantId: 4,
-    location: "Bislett",
-    imgSrc: "",
-    openingHours: "",
-    telephone: 90901090,
-  },
-];
-export default restaurants;
+const RestaurantModule = (function(){
+
+  const restaurants = [
+      {
+        restaurantId: 1,
+        location: "Karl Johan",
+        image: "",
+        openingHours: "",
+        telephone: 71582859,
+      },
+      {
+        restaurantId: 2,
+        location: "Operaen",
+        image: "",
+        openingHours: "",
+        telephone: 92938283,
+      },
+      {
+        restaurantId: 3,
+        location: "Stovner",
+        image: "",
+        openingHours: "",
+        telephone: 60003005,
+      },
+      {
+        restaurantId: 4,
+        location: "Bislett",
+        image: "",
+        openingHours: "",
+        telephone: 90901090,
+      }
+    ];
+
+    const getAll = () => restaurants;
+
+    return {
+        getAll
+    }
+
+} ())
+
+export default RestaurantModule;

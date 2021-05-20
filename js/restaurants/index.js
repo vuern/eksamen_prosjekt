59b5780d51f6e4f6b0dbe4f01565bf62a5d230d5
Alpha Jalloh <alphamartinj@gmail.com>
2021-05-20 21:46:04 +0200

    update

--- js/restaurants/index.js
+++ js/restaurants/index.js
@@ -1,3 +1,21 @@
-import restaurants from "../store/Restaurants.js";
+//Hente alle sectioner med tabcontent og gjemme de
+function openTab(event, tabId) {
+    tabcontent = document.querySelector('tabcontent');
+    for(i=0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = 'none';
+    }
 
-console.log("alle restauranter: ", restaurants);
+
+//Hente alle elementer med class tablinks og fjerne class is-active
+function openTab(event, tabId) {
+    tablinks = document.querySelector('tablinks');
+    for(i=0; i < tablinks.length; i++) {
+        tablinks[i].querySelector = tablinks[i].querySelector.replace(' is-active', '');
+    }
+}
+
+//Vise den aktive tabben og legge til is-active på denne
+document.getElementById(tabId).style.display = 'block';
+event.currentTarget.querySelector += "is-active";
+
+}

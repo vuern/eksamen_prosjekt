43f513f866d0fd3bd07ac7fc5025fc22b848d4a8
Arber Krasniqi <arkr007@student.kristiania.no>
2021-05-20 18:45:06 +0200

    åpningstider

--- js/store/Restaurants.js
+++ js/store/Restaurants.js
@@ -4,29 +4,29 @@ const RestaurantModule = (function(){
       {
         restaurantId: 1,
         location: "Karl Johan",
-        image: "",
-        openingHours: "",
+        image: "1.jpg",
+        openingHours: "11:00-23:00",
         telephone: 71582859,
       },
       {
         restaurantId: 2,
         location: "Operaen",
-        image: "",
-        openingHours: "",
+        image: "2.jpg",
+        openingHours: "11:00-23:00",
         telephone: 92938283,
       },
       {
         restaurantId: 3,
         location: "Stovner",
-        image: "",
-        openingHours: "",
+        image: "3.png",
+        openingHours: "11:00-23:00",
         telephone: 60003005,
       },
       {
         restaurantId: 4,
         location: "Bislett",
-        image: "",
-        openingHours: "",
+        image: "4.png",
+        openingHours: "11:00-23:00",
         telephone: 90901090,
       }
     ];

aca7248d04d5bdd693976b783e844086d0a6bbcc
Alpha Jalloh <alphamartinj@gmail.com>
2021-05-21 10:59:17 +0200

    det funker

--- js/restaurants/index.js
+++ js/restaurants/index.js
@@ -1,21 +1,19 @@
-//Hente alle sectioner med tabcontent og gjemme de
-function openTab(event, tabId) {
-    tabcontent = document.querySelector('tabcontent');
-    for(i=0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = 'none';
-    }
+// tabs
+const tabs = document.querySelectorAll('.tabs li');
+const tabContent = document.querySelectorAll('#tab-content > div');
 
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        tabs.forEach(item => item.classList.remove('is-active'))
+        tab.classList.add('is-active');
 
-//Hente alle elementer med class tablinks og fjerne class is-active
-function openTab(event, tabId) {
-    tablinks = document.querySelector('tablinks');
-    for(i=0; i < tablinks.length; i++) {
-        tablinks[i].querySelector = tablinks[i].querySelector.replace(' is-active', '');
-    }
-}
-
-//Vise den aktive tabben og legge til is-active på denne
-document.getElementById(tabId).style.display = 'block';
-event.currentTarget.querySelector += "is-active";
-
-}
+        const target = tab.dataset.target
+        tabContent.forEach(content => {
+            if (content.getAttribute('id') == target) {
+                content.classList.remove('is-hidden');
+            } else {
+                content.classList.add('is-hidden');
+            }
+        })
+    })
+})
